c21455f89ee520f0ae3e7cbc66d349e29ee601c8
Karthick M <kartthickmohan@gmail.com>
2022-09-29 10:11:32 +0530

    init

--- String Matching/Naive_approach.cpp
+++ String Matching/Naive_approach.cpp
@@ -1,19 +1,26 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
     string S1, S2;
-    cin>>S1>>S2;
-    int len1=S1.length(), len2=S2.length();
-    for(int i=0; i<=len1-len2; i++)
+    getline(cin, S1);
+    getline(cin, S2);
+    int len1 = S1.length(), len2 = S2.length();
+    for (int i = 0; i <= len1 - len2; i++)
     {
-        int start=i, end=i+len2;
-        if(S1[i]==S2[0])
+        int start = i, end = i + len2, f = 0;
+        if (S1[i] == S2[0])
         {
-            for(int j=0; j<len2; j++)
+            for (int j = 0; j < len2; j++)
             {
-                cout<<S1[start++]<<" "<<S2[j]<<endl;
+                if (S1[start++] != S2[j])
+                {
+                    f = 1;
+                    break;
+                }
             }
+            if (f == 0)
+                printf("Pattern found at index %d", i);
         }
     }
 }
