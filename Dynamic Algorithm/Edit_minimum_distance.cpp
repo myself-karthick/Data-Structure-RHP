5372a437662bc5bb3e9a48b479007a036f1a04cd
Karthick M <kartthickmohan@gmail.com>
2022-10-08 11:59:19 +0530

    init

---
+++ Dynamic Algorithm/Edit_minimum_distance.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    string S1, S2;
+    cin>>S1>>S2;
+    int c=0;
+    int arr1[26]={0}, arr2[26]={0};
+    for(int i=0; S1[i]; i++)
+    {
+        arr1[S1[i]-97];
+    }
+    for(int i=0; S2[i]; i++)
+    {
+        arr2[S2[i]-97];
+    }
+    for(int i=0; i<26; i++)
+    {
+        if(arr1[i]!=0 && arr2[i]!=0)
+        {
+            c+=abs(arr1[i]-arr2[i]);
+        }
+    }
+    cout<<c;
+}
