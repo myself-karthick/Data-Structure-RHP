1f154d7d83c7ce08297ef560873f298181b4fd19
Karthick M <kartthickmohan@gmail.com>
2022-10-12 11:33:12 +0530

    init

---
+++ Others/Problem.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string str;
+    char temp[5];
+    getline(cin, str);
+    map<int, int> years;
+    for (int i = 0; i <= str.size() - 4; i++)
+    {
+        if (isdigit(str[i]) && isdigit(str[i + 1]) && isdigit(str[i + 2]) && isdigit(str[i + 3]))
+        {
+            temp[0] = str[i];
+            temp[1] = str[i + 1];
+            temp[2] = str[i + 2];
+            temp[3] = str[i + 3];
+            int year = atoi(temp);
+            years[year]++;
+        }
+    }
+    cout << years.size();
+}
